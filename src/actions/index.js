49e889a173d66f5c44857084a8b51bb74eb32a36
Andrew Crawford <andrew.crawford.za@gmail.com>
2018-03-26 12:42:06 +0200

    continue sign user in

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,21 +1,26 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
+import { AUTH_USER } from './types';
 
 const routeUrl = 'http://localhost:3090';
 
 export function signinUser({ email, password }) {
-  return function (dispatch) {
+  return (dispatch) => {
     // sub email to server
-    axios.post(`${routeUrl}/signin`, { email, password });
+    axios.post(`${routeUrl}/signin`, { email, password })
+      .then((response) => {
+        // good
+        // - update state to say authenticated
+        dispatch({ type: AUTH_USER });
+        // - savr jwt token
+        // - redirect
+        browserHistory.push('/feature');
+      })
+      .catch(() => {
 
-
-    // good
-    // - update state to say authenticated
-    // - savr jwt token
-    // - redirect
-
-
-    // bad
-    // - show error
+        // bad
+        // - show error
+      });
   };
 }
 
